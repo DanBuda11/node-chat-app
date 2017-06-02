feb6c36101c019c77163b9192468bb32b5edf770
Dan Buda <danbuda@gmail.com>
2017-06-01 20:58:11 -0500

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 		socket.on('connect', function()  {
 			console.log('connected to server');
-
-			socket.emit('createMessage', {
-				from: 'bob',
-				text: 'Hey it\'s Dan.'
-			});
 		});
 
 		socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('new user connected');
 
-	socket.emit('newMessage', {
-		from: 'mike',
-		text: 'Hey, what is up?',
-		createdAt: 123
-	});
-
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
 	});
 
 	socket.on('disconnect', () => {
