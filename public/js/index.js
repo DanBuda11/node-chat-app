2f483cafeb75c04369aa2d246d669c809e9feb01
Dan Buda <danbuda@gmail.com>
2017-06-03 16:56:09 -0500

    start adding geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,19 @@ var socket = io();
 
 			});
 		});
+
+		var locationButton = jQuery('#send-location');
+		locationButton.on('click', function() {
+			if (!navigator.geolocation) {
+				return alert('Geolocation not supported by your browser.');
+			}
+
+			navigator.geolocation.getCurrentPosition(function(position) {
+				socket.emit('createLocationMessage', {
+					latitude: position.coords.latitude,
+					longitude: position.coords.longitude
+				});
+			}, function() {
+				alert('Unable to fetch location.');
+			});
+		});
--- server/server.js
+++ server/server.js
@@ -23,11 +23,10 @@ io.on('connection', (socket) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', generateMessage(message.from, message.text));
 		callback('This is from the server.');
-		// socket.broadcast.emit('newMessage', {
-		// 	from: message.from,
-		// 	text: message.text,
-		// 	createdAt: new Date().getTime()
-		// });
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
 	});
 
 	socket.on('disconnect', () => {
