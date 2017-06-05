d6c343a9ec677c4da1a2f737c183524cc6fa7ce3
Dan Buda <danbuda@gmail.com>
2017-06-05 12:27:23 -0500

    add geolocation via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
 		expect(message.createdAt).toBeA('number');
 		expect(message).toInclude({from, text});
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Dan';
+		var lat = 3;
+		var long = 4;
+		var url = 'https://www.google.com/maps?q=3,4';
+		var message = generateLocationMessage(from, lat, long);
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({from, url});
 	});
 });
